5c04897bafc3e4a4195866c0bbcf4a73a01bc125
Abby <ahardin7@kent.edu>
2018-11-11 19:58:41 -0500

    missed css

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
 
-def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+def aboutme (request):
+    return render(request, 'blog/aboutme.html', {})
+    
