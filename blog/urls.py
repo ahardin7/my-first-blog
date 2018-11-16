da07ffbbca4c42e37ab42a34b782ce2ad0f6cba9
Abby <ahardin7@kent.edu>
2018-11-16 11:27:10 -0500

    changed pictures

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('about_me', views.about_me, name='about_me'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 def post_list (request):
     return render(request, 'blog/post_list.html', {})
+
+def about_me (request):
+    return render(request, 'blog/about_me.html', {})
     
